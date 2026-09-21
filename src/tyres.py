b3e7a05941394601e8270ad785dda82d39638205
dlcbeatrix <dlcbtrc@gmail.com>
2026-09-21 22:07:10 +0200

    Add docs: screenshot for demonstration

--- src/tyres.py
+++ src/tyres.py
@@ -8,7 +8,7 @@ def clean_laps(laps: pd.DataFrame, driver: str, fuel_effect: float = 0.03, min_s
     driver_laps = driver_laps[driver_laps["LapTime"].notna()]                                       #laps with no time
     driver_laps = driver_laps[driver_laps["PitInTime"].isna() & driver_laps['PitOutTime'].isna()]   #remove in and out-laps
     driver_laps = driver_laps[driver_laps["TrackStatus"]== "1"]                                     #green track
-    driver_laps = driver_laps[driver_laps['LapNumber']> 1]
+    driver_laps = driver_laps[driver_laps['LapNumber']>1]
     
     driver_laps['LapTimeSec'] = driver_laps['LapTime'].dt.total_seconds()   #lap time in seconds
     
